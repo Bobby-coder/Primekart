924d423c61b2432b79345f7f45e8f1426a57c6e1
Bobby-coder <bobbysadhwani612@gmail.com>
2024-10-17 22:18:45 +0530

    lazy loading added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,37 @@
+import React, { Suspense, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ProductsPage from "./pages/ProductsPage";
-import ProductDetailPage from "./pages/ProductDetailPage";
-import CartPage from "./pages/CartPage";
-import WishlistPage from "./pages/WishlistPage";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCart } from "./store/features/cart/cartSlice";
+import { fetchWishlist } from "./store/features/wishlist/wishlistSlice";
+import { fetchUserDetails } from "./store/features/auth/authSlice";
+import ScrollToTop from "./components/ScrollToTop";
 import Footer from "./components/Footer";
 import Header from "./components/header/Header";
 import CategoryNavbar from "./components/home/CategoryNavbar";
-import SearchPage from "./pages/SearchPage";
-import ScrollToTop from "./components/ScrollToTop";
-import PageNotFound from "./pages/PageNotFound";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import VerificationPage from "./pages/VerificationPage";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { fetchUserDetails } from "./store/features/auth/authSlice";
-import MyAccountPage from "./pages/MyAccountPage";
-import { fetchCart } from "./store/features/cart/cartSlice";
-import { fetchWishlist } from "./store/features/wishlist/wishlistSlice";
-import ResetPasswordPage from "./pages/ResetPasswordPage";
 import Protected from "./components/Protected";
+
+// Lazy load page components
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const ProductsPage = React.lazy(() => import("./pages/ProductsPage"));
+const ProductDetailPage = React.lazy(() => import("./pages/ProductDetailPage"));
+const CartPage = React.lazy(() => import("./pages/CartPage"));
+const WishlistPage = React.lazy(() => import("./pages/WishlistPage"));
+const SearchPage = React.lazy(() => import("./pages/SearchPage"));
+const LoginPage = React.lazy(() => import("./pages/LoginPage"));
+const SignupPage = React.lazy(() => import("./pages/SignupPage"));
+const VerificationPage = React.lazy(() => import("./pages/VerificationPage"));
+const MyAccountPage = React.lazy(() => import("./pages/MyAccountPage"));
+const ResetPasswordPage = React.lazy(() => import("./pages/ResetPasswordPage"));
+const PageNotFound = React.lazy(() => import("./pages/PageNotFound"));
 
 function App() {
   const dispatch = useDispatch();
   const { name } = useSelector((state) => state.auth);
 
-  // Whenever app mounts, initialize user details, cart and wishlist only if it is logged in
+  // Whenever app mounts, initialize user details, cart, and wishlist if logged in
   useEffect(() => {
+    dispatch(fetchUserDetails());
     if (name) {
-      dispatch(fetchUserDetails());
       dispatch(fetchCart());
       dispatch(fetchWishlist());
     }
@@ -40,44 +42,49 @@ function App() {
       <ScrollToTop />
       <Header />
       <CategoryNavbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/category/:name/" element={<ProductsPage />} />
-        <Route
-          path="/category/:name/:productId"
-          element={<ProductDetailPage />}
-        />
-        <Route
-          path="/cart"
-          element={
-            <Protected>
-              <CartPage />
-            </Protected>
-          }
-        />
-        <Route
-          path="/wishlist"
-          element={
-            <Protected>
-              <WishlistPage />
-            </Protected>
-          }
-        />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
-        <Route path="/verification" element={<VerificationPage />} />
-        <Route
-          path="/my-account"
-          element={
-            <Protected>
-              <MyAccountPage />
-            </Protected>
-          }
-        />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/category/:name/" element={<ProductsPage />} />
+          <Route
+            path="/category/:name/:productId"
+            element={<ProductDetailPage />}
+          />
+          <Route
+            path="/cart"
+            element={
+              <Protected>
+                <CartPage />
+              </Protected>
+            }
+          />
+          <Route
+            path="/wishlist"
+            element={
+              <Protected>
+                <WishlistPage />
+              </Protected>
+            }
+          />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route
+            path="/reset-password/:token"
+            element={<ResetPasswordPage />}
+          />
+          <Route path="/verification" element={<VerificationPage />} />
+          <Route
+            path="/my-account"
+            element={
+              <Protected>
+                <MyAccountPage />
+              </Protected>
+            }
+          />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </>
   );
